0ab5d4c14ece4e5aac01914f4877edd47b14eaf7
djeloul abderrezague <mohamed.abderrezague@gmail.com>
2020-05-25 01:04:04 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 import Navbar from "./components/Navbar";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "#b22a00",
+      contrastText: "#fff",
+    },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
       <Router>
         <Navbar />
         <div className="container">
@@ -21,6 +41,7 @@ function App() {
         </div>
       </Router>
     </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import Scream from '../components/Scream';
+
 
 class home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>home page</h1>
-            </div>
-        )
-    }
+  state = {
+    screams: null,
+  };
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          screams: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+  render() {
+    let recentScreamMarkup = this.state.screams ? (
+      this.state.screams.map((scream) => <Scream scream={scream}/>)
+    ) : (
+      <p>loading...</p>
+    );
+    return (
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>profile...</p>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-export default home
+export default home;
