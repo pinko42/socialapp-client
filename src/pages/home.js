ec789b6100a8e0fa9c5c52871bc839951ae1241c
djeloul abderrezague <mohamed.abderrezague@gmail.com>
2020-06-06 00:42:46 +0100

    commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -21,7 +21,7 @@ class home extends Component {
   }
   render() {
     let recentScreamMarkup = this.state.screams ? (
-      this.state.screams.map((scream) => <Scream scream={scream}/>)
+      this.state.screams.map((scream) => <Scream key={scream.screamId} scream={scream}/>)
     ) : (
       <p>loading...</p>
     );
