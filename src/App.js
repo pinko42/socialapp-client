6ad34e9eb6edd21d794544abda6e935a36cfffb0
djeloul abderrezague <mohamed.abderrezague@gmail.com>
2020-06-09 00:53:23 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,55 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import themefile from "./util/theme";
+import jwtDecode from "jwt-decode";
+import AuthRoute from "./util/authRoute";
 
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 import Navbar from "./components/Navbar";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#33c9dc",
-      main: "#00bcd4",
-      dark: "#008394",
-      contrastText: "#fff",
-    },
-    secondary: {
-      light: "#ff6333",
-      main: "#ff3d00",
-      dark: "#b22a00",
-      contrastText: "#fff",
-    },
-  },
-});
+const theme = createMuiTheme(themefile);
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <AuthRoute
+                exact
+                path="/login"
+                component={login}
+                authenticated={authenticated}
+              />
+              <AuthRoute
+                exact
+                path="/signup"
+                component={signup}
+                authenticated={authenticated}
+              />
+            </Switch>
+          </div>
+        </Router>
+      </div>
     </MuiThemeProvider>
   );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -11,32 +11,9 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-const styles = {
-  form: {
-    textAlign: "center",
-  },
-  image: {
-    margin: "20px auto 20px auto",
-  },
-  pageTitle: {
-    margin: "10px auto 10px auto",
-  },
-  textField: {
-    margin: "10px auto 10px auto",
-  },
-  button: {
-    marginTop: 20,
-    position: "relative",
-  },
-  customError: {
-    color: "red",
-    fontSize: "0.8rem",
-    marginTop: 10,
-  },
-  progress: {
-    position: "absolute",
-  },
-};
+const styles = (theme) => ({
+  ...theme.spreadThis
+})
 
 class login extends Component {
   constructor() {
@@ -61,6 +38,7 @@ class login extends Component {
       .post("/login", userData)
       .then((res) => {
         console.log(res.data);
+        localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
         this.setState({
           loading: false,
         });
@@ -115,6 +93,7 @@ class login extends Component {
               onChange={this.handleChange}
               fullWidth
             />
+            
             {errors.general && (
               <Typography variant="body2" className={classes.customError}>
                 {errors.general}
@@ -132,7 +111,7 @@ class login extends Component {
             </Button>
             <br />
             <small>
-              dont have an account? sign up <Link to="/signup">here</Link>
+              dont have an account? signup <Link to="/signup">here</Link>
             </small>
           </form>
         </Grid>
